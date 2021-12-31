d72f23ca2352b2d6ed257da8bac1d8ccd3e50bf7
kimfarhat <kf219@ic.ac.uk>
2021-12-31 17:27:05 +0200

    junior doctor tests all done -

--- src/main/java/Entities/JuniorDoctor.java
+++ src/main/java/Entities/JuniorDoctor.java
@@ -3,17 +3,20 @@ package Entities;
 import java.util.ArrayList;
 
 public class JuniorDoctor extends Doctor {
+
+    SingletonDatabase db = SingletonDatabase.getInstance();
+
     public JuniorDoctor(String nameIn, String DOBIn, String sexIn, String emailIn, String numPagerIn){
         super(nameIn, DOBIn, sexIn, emailIn, numPagerIn);
     }
 
-    public void takeUpTask(String task){
+    public ArrayList<Task> takeUpTask(Doctor doc){
         //assigns task to themselves
-        return;
+        return db.getDoctorTasks(doc);
     }
 
     public ArrayList<Task> getTasks(){
         //Returns all task available to junior doctor, by calling the task model
-        return null;
+        return db.getCurrTaskList();
     }
 }
